d415db66530d904e28897e82d69a2babc558480b
Daniel Svitan <dankosaurus9@gmail.com>
2021-03-25 12:59:55 +0100

    :bug: new cookie bug

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import { useState } from "react";
-import { getCookie, setCookie } from "./cookieManager";
+import { useCookies } from "react-cookie";
 // Components
 import { Task } from "./components/task";
 
+
 const App = () => {
 	const [tasks, setTasks] = useState([]);
+	const [cookies, setCookie, removeCookie] = useCookies(["data"]);
 
-	const DeleteTask = (task) => {
+	const getCookie = (name) => {
+		return name === "*" ? cookies : cookies[name];
+	}
+
+	const deleteTask = (task) => {
 		if (tasks.includes(task)) {
 			console.log("Deleting task " + task + "...");
-			setTasks(tasks.splice(tasks.indexOf(task), 1));
+			const updatedTasks = tasks.splice(tasks.indexOf(task), 1);
+			setCookie("tasks", JSON.stringify(updatedTasks))
+			setTasks(updatedTasks);
 		}
 	}
 
 	const loadTasks = () => {
 		console.log("Loading tasks...");
 
-		const tasksInCookies = JSON.parse(getCookie("tasks")) || [];
-
-		for (const taskData of tasksInCookies) {
-			const currentTask = new Task(taskData.name, taskData.description, DeleteTask);
+		for (const taskData of JSON.parse(getCookie("tasks"))) {
+			const currentTask = new Task(taskData.name, taskData.description, deleteTask);
 			let updatedTasks = tasks;
 			updatedTasks.push(currentTask);
 			setTasks(updatedTasks);
@@ -29,7 +35,7 @@ const App = () => {
 	const saveTasks = () => {
 		console.log("Saving tasks...");
 
-		let tasksToBeWrittenInCookies = [];
+		/*let tasksToBeWrittenInCookies = [];
 
 		for (const t of tasks) {
 			tasksToBeWrittenInCookies.push({
@@ -38,7 +44,7 @@ const App = () => {
 			});
 		}
 
-		setCookie("tasks", JSON.stringify(tasksToBeWrittenInCookies));
+		setCookie("tasks", JSON.stringify(tasksToBeWrittenInCookies));*/
 	}
 
 	window.onload = loadTasks();
